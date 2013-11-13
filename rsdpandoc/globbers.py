7b9d5c1208a5e251952047ec6f0028c009fefb6f
James Hetherington <j.hetherington@ucl.ac.uk>
2013-11-13 19:59:53 +0000

    Enable movable asset sources

--- rsdpandoc/globbers.py
+++ rsdpandoc/globbers.py
@@ -59,33 +59,42 @@ def default_latex_assets(dependants,target,env):
 def default_web_assets(dependants,target,env):
 	asset_glob(local_assets,'.',dependants,env.Cp,'*.html',env)
 
-def reveal_assets(dependants,env):
-	assets_glob('asset_sources','reveal/assets',dependants,env)
-	default_reveal_assets(dependants,'reveal/assets',env)
+def reveal_assets(dependants,env,target="reveal",sources="asset_sources"):
+	assets_glob(sources,os.path.join(target,'assets'),dependants,env)
+	default_reveal_assets(dependants,os.path.join(target,'assets'),env)
 
-def latex_assets(dependants,env):
-	assets_glob('asset_sources','assets',dependants,env)
+def latex_assets(dependants,env,target="pdf",sources="asset_sources"):
+	assets_glob(sources,'assets',dependants,env)
 	default_latex_assets(dependants,'assets',env)
 
-def web_assets(dependants,env):
-	assets_glob('asset_sources','web/assets',dependants,env)
-	default_web_assets(dependants,'web/assets',env)
+def web_assets(dependants,env,target="web",sources="asset_sources"):
+	assets_glob('asset_sources',os.path.join(target,'assets'),dependants,env)
+	default_web_assets(dependants,os.path.join(target,'assets'),env)
 
-def reveal_layout(sources,env):
-	slides=env.PandocSlides('reveal/index.html',sources)
-	reveal_assets(slides,env)
+def reveal_layout(sources,env,target="reveal",asset_sources="asset_sources"):
+	slides=env.PandocSlides(os.path.join(target,'index.html'),sources)
+	reveal_assets(slides,env,target,asset_sources)
 	return slides
 
-def latex_layout(sources,env):
-	document=env.PandocLatex('pdf/document.pdf',sources)
-	tex=env.PandocLatex('pdf/document.tex',sources)
-	latex_assets([document,tex],env)
+def latex_layout(sources,env,target="pdf",asset_sources="asset_sources"):
+	document=env.PandocLatex(os.path.join(target,'document.pdf'),sources)
+	tex=env.PandocLatex(os.path.join(target,'document.tex'),sources)
+	latex_assets([document,tex],env,target,asset_sources)
 	return document
 
-def web_layout(sources,env):
-	document=env.PandocJekyll('web/index.html',sources)
-	web_assets(document,env)
+def web_layout(sources,env,target="web",asset_sources="asset_sources"):
+	document=env.PandocJekyll(os.path.join(target,'index.html'),sources)
+	web_assets(document,env,target,asset_sources)
 	return document
 
-def standard_layout(sources,env):
-	return[latex_layout(sources,env),reveal_layout(sources,env)]
+def mixed_html_layout(sources,env,asset_sources="asset_sources"):
+	return[
+		web_layout(sources,env,'html',asset_sources),
+		reveal_layout(sources,env,'html/reveal',asset_sources)
+	]
+
+def standard_layout(sources,env,asset_sources="asset_sources"):
+	return[
+		latex_layout(sources,env,"pdf",asset_sources),
+		reveal_layout(sources,env,"reveal",asset_sources)
+	]
