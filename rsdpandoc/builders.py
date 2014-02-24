1f26cf9fa30279d2d9322449743f33a8d0c84c42
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-02-24 18:10:09 +0000

    Search for executables using WhereIs
    Not sure how shelling out is done, but otherwise can't find pandoc on my system. Maybe user vs
    global install problem.

--- rsdpandoc/builders.py
+++ rsdpandoc/builders.py
@@ -33,102 +33,109 @@ SlideExtraCSSFiles = []
 SlideTemplateName = False
 
 def add_builders(env): 
+    from sys import executable
+    from SCons.Util import WhereIs
+    
+    pandoc_exec = WhereIs('pandoc')
+    dot_exec = WhereIs('dot')
+    assets = ' '.join([ "--css=assets/" + str(x) for x in SlideExtraCSSFiles ])
+    template = ("--template=" + SlideTemplateName) if SlideTemplateName else ' '
 
-	env.Append(BUILDERS={
-
-		'WSD':env.Builder(
-			action=['python -m websequence --style magazine '+
-				'--format=png --in $SOURCE --out $TARGET'],
-	    	suffix='.png',
-	    	src_suffix='.wsd'),
-	    
-	    'Dot':env.Builder(
-	    	action=["dot -Tpng $SOURCE -o $TARGET"], 
-			suffix=".png", 
-			src_suffix=".dot"),
-		
-		'Neato':env.Builder(
-			action=["neato -Tpng $SOURCE -o $TARGET"], 
-			suffix=".png", 
-			src_suffix=".dot"),
-		
-		'Python':env.Builder(
-			action=["python $SOURCE $TARGET"],
-	    	suffix='.png',
-	    	src_suffix='.py'),
-	    
-	    'Shell':env.Builder(
-	    	action=['bash $SOURCE $TARGET'],
-	    	suffix='.png',
-	    	src_suffix='.sh'),
-	    
-	    'Wget':env.Builder(
-	    	action=wget_each_url,
-	    	emitter=yaml_emitter),
-
+    env.Append(BUILDERS={
+    
+        'WSD':env.Builder(
+        	action=['{0} -m websequence --style magazine '.format(executable) +
+        		'--format=png --in $SOURCE --out $TARGET'],
+        suffix='.png',
+        src_suffix='.wsd'),
+        
+        'Dot':env.Builder(
+        	action=["{0} -Tpng $SOURCE -o $TARGET".format(dot_exec)], 
+    		suffix=".png", 
+    		src_suffix=".dot"),
+    	
+    	'Neato':env.Builder(
+    		action=["neato -Tpng $SOURCE -o $TARGET"], 
+    		suffix=".png", 
+    		src_suffix=".dot"),
+    	
+    	'Python':env.Builder(
+    		action=["{0} $SOURCE $TARGET".format(executable)],
+        	suffix='.png',
+        	src_suffix='.py'),
+        
+        'Shell':env.Builder(
+        	action=['bash $SOURCE $TARGET'],
+        	suffix='.png',
+        	src_suffix='.sh'),
+        
+        'Wget':env.Builder(
+        	action=wget_each_url,
+        	emitter=yaml_emitter),
+    
         'PandocSlides': env.Builder(
-            generator = lambda source,target,env,for_signature: (
-                         'pandoc -t revealjs -s -V theme=%s'+
-                         ' --css=assets/%s.css'             +
-                         ' --css=assets/%s-ucl-overlay.css'     +
-                         ' --css=assets/local_styles.css'   +
-                         ' %s '                             +
-                         ' %s '                             +
-                         ' --default-image-extension=png'   +
-                         ' --highlight-style=%s'            +
-                         ' --mathjax'                       +
-                         ' -V revealjs-url=http://lab.hakim.se/reveal-js/'+
-                         ' %s '                             +
-                         '-o %s' ) %
-                         ( SlideStyle, SlideStyle, SlideStyle,
-                           ("--template="+SlideTemplateName if SlideTemplateName else ' '),
-                           ' '.join([ "--css=assets/%s" % x for x in SlideExtraCSSFiles ]),
-                           SlideSyntaxHighlightingStyle,
-                           ' '.join([str(x) for x in source]), 
-                           target[0]
+            generator = lambda source, target, env, for_signature: (
+                           '{pandoc} -t revealjs -s -V theme={theme}'        \
+                           ' --css=assets/{theme}.css'                       \
+                           ' --css=assets/{theme}-ucl-overlay.css'           \
+                           ' --css=assets/local_styles.css'                  \
+                           ' {template} '                                    \
+                           ' {assets} '                                      \
+                           ' --default-image-extension=png'                  \
+                           ' --highlight-style={highlight}'                  \
+                           ' --mathjax'                                      \
+                           ' -V revealjs-url=http://lab.hakim.se/reveal-js/' \
+                           ' {sources} '                                     \
+                           '-o {output}'.format(
+                             pandoc    = pandoc_exec,
+                             theme     = SlideStyle, 
+                             template  = template,
+                             assets    = assets,
+                             highlight = SlideSyntaxHighlightingStyle,
+                             sources   = ' '.join([str(x) for x in source]), 
+                             output    = target[0]
                            )
-            ),
- 
-		'PandocLatex':env.Builder(
-			action=['pandoc --template=report '+
-					'-V documentclass=scrartcl ' +
-					'-V links-as-notes ' +
-					'--filter pandoc-citeproc ' +
-					'--default-image-extension=png '+
-					'-V linkcolor="uclmidgreen" ' +
-					'--number-sections $SOURCES -o $TARGET'],
-			),
-
-		'PandocJekyll':env.Builder(
-			action=['pandoc -thtml -s --template=jekyll'+
-					' --default-image-extension=png'+
-					' --mathjax '+
-					' $SOURCES -o $TARGET']
-			),
-		
-		'Browse':env.Builder(
-			action=browse_each_url,
-	        emitter=yaml_emitter),
-		
-		'Carousel':env.Builder(
-			action=carousel.carousel_action, 
-	    	suffix=".png",
-	    	src_suffix=".carousel"),
-	    
-	    'Cp':env.Builder(
-	    	action=["cp $SOURCES $TARGET"]
-	    	),
-
-	    'R': env.Builder(
-	    	action=['RScript $SOURCE $TARGET']
-	    	), # Assume R script expects dest file as first clarg
-
-	    'Bib': env.Builder(
-	    	action=tidybib.tidybib_action,
-	    	suffix=".bib",
-	    	source_suffix=".zbib"
-	    	)
-		})
-
-
-	env['ENV']["PYTHONPATH"]=sys.path
+            )
+        ),
+    
+        'PandocLatex':env.Builder(
+        	action=['{0} --template=report '.format(pandoc_exec) +
+        			'-V documentclass=scrartcl ' +
+        			'-V links-as-notes ' +
+        			'--filter pandoc-citeproc ' +
+        			'--default-image-extension=png '+
+        			'-V linkcolor="uclmidgreen" ' +
+        			'--number-sections $SOURCES -o $TARGET'],
+        	),
+    
+        'PandocJekyll':env.Builder(
+        	action=['{0} -thtml -s --template=jekyll'.format(pandoc_exec)+
+        			' --default-image-extension=png'+
+        			' --mathjax '+
+        			' $SOURCES -o $TARGET']
+        	),
+        
+              'Browse':env.Builder(action=browse_each_url, emitter=yaml_emitter),
+        
+        'Carousel':env.Builder(
+        	action=carousel.carousel_action, 
+            suffix=".png",
+            src_suffix=".carousel"),
+        
+        'Cp':env.Builder(
+        	action=["cp $SOURCES $TARGET"]
+        	),
+    
+        'R': env.Builder(
+        	action=['RScript $SOURCE $TARGET']
+        	), # Assume R script expects dest file as first clarg
+    
+        'Bib': env.Builder(
+        	action=tidybib.tidybib_action,
+        	suffix=".bib",
+        	source_suffix=".zbib"
+        )
+          })
+    
+    
+    env['ENV']["PYTHONPATH"]=sys.path
